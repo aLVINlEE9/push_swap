7a7b3f0eab13fc183d460092ae253b4771083768
aLVINlEE9 <fldh3746@gmail.com>
2021-09-13 20:26:29 +0900

    push_swap

--- push_swap/includes/push_swap.h
+++ push_swap/includes/push_swap.h
@@ -6,13 +6,15 @@
 /*   By: seungsle <seungsle@student.42seoul.kr      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/11 17:16:17 by seungsle          #+#    #+#             */
-/*   Updated: 2021/09/11 17:19:14 by seungsle         ###   ########.fr       */
+/*   Updated: 2021/09/13 19:38:47 by seungsle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
 
+# define TRUE 1
+# define FALSE 0
 # include <stdlib.h>
 # include <unistd.h>
 
--- push_swap/srcs/push_swap.c
+++ push_swap/srcs/push_swap.c
@@ -6,13 +6,43 @@
 /*   By: seungsle <seungsle@student.42seoul.kr      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/11 17:15:38 by seungsle          #+#    #+#             */
-/*   Updated: 2021/09/11 17:20:03 by seungsle         ###   ########.fr       */
+/*   Updated: 2021/09/13 20:26:14 by seungsle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/push_swap.h"
 
+int	print_error(char *str)
+{
+	write(1, "\n", 1);
+	write(2, str, ft_strlen(str));
+	return (FALSE);
+}
+
+int vaild_arg(int argc, char **argv)
+{
+	int	i;
+	int	num;
+
+	i = 0;
+	if (argc !=2)
+		return (print_error("[ERROR]bad arguments : not one argument!\n"));
+	while (argv[1][i++])
+	{
+		if (32 != argv[1][i])
+			continue;
+		if (!(48 <= argv[1][i] && argv[1][i] <= 57))
+			return (print_error("[ERROR]bad arguments : not number!\n"));
+		else
+			return (TRUE);
+	}
+}
+
 int main(int argc, char **argv)
 {
+	int	**arr;
 
+	if (!valid_arg(argc, argv))
+		return (0);
+	num_arr = parsing_num(argv);
 }

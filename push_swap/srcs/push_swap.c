f6b325fb3ec57942343a9f76927e9ffb90b55ee5
aLVINlEE9 <fldh3746@gmail.com>
2021-09-13 21:08:35 +0900

    push_swap

--- push_swap/srcs/push_swap.c
+++ push_swap/srcs/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: seungsle <seungsle@student.42seoul.kr      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/11 17:15:38 by seungsle          #+#    #+#             */
-/*   Updated: 2021/09/13 20:26:14 by seungsle         ###   ########.fr       */
+/*   Updated: 2021/09/13 21:08:25 by seungsle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,10 +19,16 @@ int	print_error(char *str)
 	return (FALSE);
 }
 
-int vaild_arg(int argc, char **argv)
+void	set_node(t_dlist *node, int	*num_ptr)
+{
+	
+}
+
+int vaild_arg(int argc, char **argv, t_dlist *A_node)
 {
 	int	i;
 	int	num;
+	int	*num_ptr;
 
 	i = 0;
 	if (argc !=2)
@@ -34,15 +40,30 @@ int vaild_arg(int argc, char **argv)
 		if (!(48 <= argv[1][i] && argv[1][i] <= 57))
 			return (print_error("[ERROR]bad arguments : not number!\n"));
 		else
-			return (TRUE);
+			i += parsing_num(argv[1], i, num_ptr);
+		set_node(A_node, num_ptr);
+		free(num_ptr);
+	}
+}
+
+int	parsing_num(char **argv, int index, int	*num_ptr)
+{
+	int	i;
+
+	i = index;
+	while (argv[1][index++])
+	{
+		if (32 != argv[1][index])
+			continue;
 	}
 }
 
 int main(int argc, char **argv)
 {
-	int	**arr;
+	t_dlist	*A_node;
+	t_dlist	*B_node;
 
-	if (!valid_arg(argc, argv))
+	if (!valid_arg(argc, argv, A_node))
 		return (0);
 	num_arr = parsing_num(argv);
 }

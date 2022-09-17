a9389a1e7abb745bed5215722c89e8fcca0d62d7
aLVINlEE9 <fldh3746@gmail.com>
2022-09-17 15:21:17 +0900

    change execute file naem

--- push_swap_new/tester.sh
+++ push_swap_new/tester.sh
@@ -6,10 +6,10 @@ SUM=0
 NUM_OVER=0
 MAX=$LIMIT
 ARG=`jot -r $CNT -2147483648 2147483647 | tr "\n" " "`
-NUM=`./sort $ARG | wc -l`
+NUM=`./push_swap $ARG | wc -l`
 while [ $INDEX -lt $ITER ]
 do
-echo "$INDEX	:	$NUM `./sort $ARG | ./checker $ARG`"
+echo "$INDEX	:	$NUM `./push_swap $ARG | ./checker $ARG`"
 if [ $NUM -ge $LIMIT ]; then
 NUM_OVER=`expr $NUM_OVER + 1`
 fi
@@ -18,7 +18,7 @@ MAX=$NUM
 fi
 SUM=`expr $SUM + $NUM`
 ARG=`jot -r $CNT -2147483648 2147483647 | tr "\n" " "`
-NUM=`./sort $ARG | wc -l`
+NUM=`./push_swap $ARG | wc -l`
 INDEX=`expr $INDEX + 1`
 done
 echo "$NUM_OVER overred operations for $ITER times (Average: `expr $SUM / $ITER`)"
